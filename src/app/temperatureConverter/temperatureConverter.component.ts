5f960917dab23b64c0a3eea1b33b3919df492766
Alchemist Bounty <alchemist.bounty@protonmail.com>
2021-11-17 20:10:22 -0500

    update conversion

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -7,10 +7,21 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 
 export class TemperatureConverter implements OnInit {
-
+  Celsius: string = '';
+  Fahrenheit: string = '';
   ngOnInit() {
     // C = (F − 32) × 5/9
     // F = C*9/5 + 32
+  }
+
+  celsiusChange(value: any) {
+    this.Celsius = value;
+    this.Fahrenheit = (parseFloat(this.Celsius) * 9 / 5 +32).toFixed(1);
+  }
+
+  fahrenheitChange(value: any) {
+    this.Fahrenheit = value;
+    this.Celsius = ((parseFloat(this.Fahrenheit)-32) * 5 / 9).toFixed(1);
   }
 
 }
